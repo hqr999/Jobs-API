091c9c1e71d26302e2ff855e437b61bca8aa9282
Henrique Queiroz <henrique.queiroz@protack.local>
2025-11-18 14:25:50 -0300

    Token generation now being handled by the models/User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -29,5 +30,10 @@ UserSchema.pre('save',async function(next){
     this.password = await bcrypt.hash(this.password,salt)
     next()
 })
+
+
+UserSchema.methods.createJWT = function() {
+    return jwt.sign({userdId:this._id,name:this.name},'jwtSecret',{expiresIn:'30d'})
+}
 
 module.exports = mongoose.model('User',UserSchema)
