8efbd8be2cc9243c1abf1b38f9e1d10a4b3502d2
Henrique Queiroz <henrique.queiroz@protack.local>
2025-11-19 15:11:23 -0300

    Get single job and update job controllers made

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -9,7 +9,19 @@ const getAllJobs = async(req,res) => {
 }
 
 const getJob = async(req,res) => {
-    res.send('pega um emprego')
+    const {
+        user: {userId},
+        params: {id: empregoId}
+    } = req 
+    
+    const emprego = await Job.findOne({
+        _id: empregoId,
+        criado_por: userId,
+    })
+    if(!emprego){
+        throw new NotFoundError(`Nenhuma ocupacao com esse ${empregoId}`)
+    }
+    res.status(StatusCodes.OK).json({ emprego })
 }
 
 const createJob = async(req,res) => {
@@ -19,7 +31,17 @@ const createJob = async(req,res) => {
 }
 
 const updateJob = async(req,res) => {
-    res.send('emprego atualizado')
+    const{body:{empresa,ocupacao},user:{userId},params:{id: empregoId}} = req 
+
+    if(empresa === '' || ocupacao === ''){
+        throw new BadRequestError('Campos Empresa ou Ocupacao ')
+    }
+
+    const emprego = await Job.findByIdAndUpdate({_id:empregoId,criado_por:userId},req.body,{new: true, runValidators: true})
+    if(!emprego){
+        throw new NotFoundError(`Nenhuma ocupacao com o id ${empregoId}`)
+    }
+    res.status(StatusCodes.OK).json({emprego})
 }
 
 const deleteJob = async(req,res) => {
